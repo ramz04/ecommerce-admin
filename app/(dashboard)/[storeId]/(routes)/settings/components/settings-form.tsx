9492e128e461e1f4d6ff4a2f4c86c6e4693eb493
ramz04 <wunpini95@gmail.com>
2023-08-15 12:38:11 +0000

    update

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -6,7 +6,7 @@ import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Store } from "@prisma/client"
 import { Trash } from "lucide-react"
-import { useForm } from "react-hook-form"
+import { set, useForm } from "react-hook-form"
 import * as z from "zod"
 import { Separator } from "@/components/ui/separator"
 import {
@@ -17,6 +17,10 @@ import {
   FormLabel,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { toast } from "react-hot-toast"
+import axios from "axios"
+import { useParams, useRouter } from "next/navigation"
+import { AlertModal } from "@/components/modals/alert-modal"
 
 interface SettingsFormProps {
   initialData: Store
@@ -31,6 +35,8 @@ type SettingsFormValues = z.infer<typeof formSchema>
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   const [open, setOpen] = useState(false)
   const [loading, setLoading] = useState(false)
+  const params = useParams()
+  const router = useRouter()
 
   const form = useForm<SettingsFormValues>({
     resolver: zodResolver(formSchema),
@@ -38,14 +44,44 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   })
 
   const onSubmit = async (data: SettingsFormValues) => {
-    console.log(data)
+    try {
+      setLoading(true)
+      await axios.patch(`/api/stores/${params.storeId}`, data)
+      router.refresh()
+      toast.success("Store updated")
+    } catch (error) {
+      toast.error("Something went wrong")
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const onDelete = async () => {
+    try {
+      setLoading(true)
+      await axios.delete(`/api/stores/${params.storeId}`)
+      router.refresh()
+      router.push("/")
+      toast.success("Store deleted")
+    } catch (error) {
+      toast.error("Make sure you removed all products and categories first")
+    } finally {
+      setLoading(false)
+      setOpen(false)
+    }
   }
 
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
       <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage store preferences" />
-        <Button variant="destructive" size="icon" onClick={() => {}}>
+        <Button variant="destructive" size="icon" onClick={() => setOpen(true)}>
           <Trash className="h-4 w-4" />
         </Button>
       </div>
