7b204a08df424479157fdba6b207a171a30a6535
ramz04 <wunpini95@gmail.com>
2023-11-20 11:31:29 +0000

    update

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Heading } from "@/components/ui/Heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@radix-ui/react-separator";
+import { Plus } from "lucide-react";
+
+const BillboardClient = () => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title="Billboards (0)"
+          description="Manage billboards for your store"
+        />
+        <Button>
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+    </>
+  );
+};
+
+export default BillboardClient;
